014f4f99ef4d1353fc8af71bafdc8bfff417f213
FredymarLeon <fredymar@gmail.com>
2025-12-09 23:30:01 -0300

    Ajustes finais do portfolio

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
+const botao = document.getElementById("btn_welcome");
+
+botao.addEventListener("click", clickedButton);
+
 function clickedButton() {
-    console.log("o botão foi clicado")
+    const paragrafo = document.querySelector(".welcome_msg");
+    const welcomeSection = document.querySelector(".welcome");
+
+    paragrafo.classList.toggle("hidden");
+
+    if (paragrafo.classList.contains("hidden")) {
+        botao.textContent = "Exibir Boas Vindas";
+        botao.classList.add("btn-ativo");
+        welcomeSection.classList.add("hidden_space");
+    } else {
+        botao.textContent = "Ocultar Boas Vindas";
+        botao.classList.remove("btn-ativo");
+        welcomeSection.classList.remove("hidden_space");
+    }    
 }
